99b8e4ad1c8eabb756a02765f916a5d4f689a431
Richard <richwats@cisco.com>
2021-02-09 09:34:29 +1100

    fixed inet contract

--- modules/apps/k8s/main.tf
+++ modules/apps/k8s/main.tf
@@ -212,12 +212,21 @@ resource "mso_schema_template_anp_epg_contract" "tf-k8s-worker-1" {
   relationship_type = "consumer"
 }
 
+# resource "mso_schema_template_anp_epg_contract" "tf-k8s-worker-2" {
+#   schema_id         = data.mso_schema.tf-hybrid-cloud.id
+#   template_name     = data.mso_schema_template.tf-hc-prod.name
+#   anp_name          = mso_schema_template_anp.tf-k8s-1.name
+#   epg_name          = mso_schema_template_anp_epg.tf-k8s-worker.name
+#   contract_name     = data.mso_schema_template_contract.tf-servers-to-inet.contract_name
+#   relationship_type = "provider"
+# }
+
 resource "mso_schema_template_anp_epg_contract" "tf-k8s-worker-2" {
   schema_id         = data.mso_schema.tf-hybrid-cloud.id
   template_name     = data.mso_schema_template.tf-hc-prod.name
   anp_name          = mso_schema_template_anp.tf-k8s-1.name
   epg_name          = mso_schema_template_anp_epg.tf-k8s-worker.name
-  contract_name     = data.mso_schema_template_contract.tf-servers-to-inet.contract_name
+  contract_name     = data.mso_schema_template_contract.tf-inet-to-k8s.contract_name
   relationship_type = "provider"
 }
 
